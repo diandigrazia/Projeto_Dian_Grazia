185faf1c421900c66c2101c30ee974b2f34c2b7c
ddigrazia <diandigrazia@gmail.com>
2019-09-15 16:04:21 -0300

    Primeiro commit

---
+++ ClasseA.java
@@ -0,0 +1,30 @@
+public class ClasseA {
+	
+	private int a1;
+	private int a2;
+	
+	public int getA1() {
+		return a1;
+	}
+	
+	public int getA2() {
+		return a2;
+	}
+	
+	public void setA1(int _a1) {
+		this.a1 = _a1;
+	}
+	
+	public void setA2(int _a2) {
+		this.a2 = _a2;
+	}
+	
+	public void MA1() {
+		System.out.printf("MA1");
+	}
+	
+	public void MA2() {
+		System.out.printf("MA2");
+	}
+}
+
---
+++ ClasseB.java
@@ -0,0 +1,30 @@
+public class ClasseB {
+	
+	private int b1;
+	private int b2;
+	
+	public int getB1() {
+		return b1;
+	}
+	
+	public int getB2() {
+		return b2;
+	}
+	
+	public void setB1(int _b1) {
+		this.b1 = _b1;
+	}
+	
+	public void setB2(int _b2) {
+		this.b2 = _b2;
+	}
+	
+	public void MB1() {
+		System.out.printf("MB1");
+	}
+	
+	public void MB2() {
+		System.out.printf("MB2");
+	}
+}
+
